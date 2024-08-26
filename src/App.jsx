d48cd791ab8611fd90c1ea168cbe26ac27b13a5a
Gideon5348 <gideonmutai096@gmail.com>
2024-08-26 18:10:16 +0300

    deplyoment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import CartPage from "./Cart";
 import ProductPage from "./product/ProductPage";
-import Shop from "./ShopPage";
+import Shop from "./Shop";
 import CheckoutPage from "./Checkout";
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
